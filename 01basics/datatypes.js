7be5aff361236910bb0a37776ef785e6feb2e995
Yuvraj Saha <114236552+Yuvraj1206@users.noreply.github.com>
2024-01-20 10:17:06 +0000

    kuch datatypes ke bare me jana jaise null undefined and object naya tha mere liye

---
+++ 01basics/datatypes.js
@@ -0,0 +1,13 @@
+let me="yuvraj";   // string
+let age=20;       // number
+let bool= false  //boolean
+let height;     //undefined
+
+//null -> standalone data type / object
+
+//object
+
+console.log(typeof me);      //string
+console.log(typeof age);
+console.log(typeof null);   //object
+console.log(typeof height); //undefined
---
+++ 01basics/variables.js
@@ -0,0 +1,14 @@
+const me= "yuvraj";
+let email= "hi@gmail.com";
+var num= 1223;            //dont use var because it is not have scope properties use let instead
+let accountState;         //declared ut not initialised
+
+color="red";
+
+// name= "abhi";  its cont variable which cant be changed 
+email="yo@gmail.com";
+num=420;
+color="blue";
+
+console.log(me);
+console.table([email, num, color, accountState]);
