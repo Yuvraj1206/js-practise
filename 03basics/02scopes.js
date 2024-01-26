4aabf40179502b337c115b61fe8328bf909b251b
Yuvraj Saha <114236552+Yuvraj1206@users.noreply.github.com>
2024-01-26 19:22:28 +0000

    function and scopes

---
+++ 03basics/02scopes.js
@@ -0,0 +1,58 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))        //calling function before its declaration
+
+function addone(num){        
+    return num + 1
+}
+
+
+
+addTwo(5)                     //calling function before its declaration will not work
+const addTwo = function(num){  //function in variable 
+    return num + 2
+}
